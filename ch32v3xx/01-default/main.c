c062c23bb96b4dbb7f1c20526b4884b16374d01e
Viacheslav Mezentsev <viacheslavmezentsev@ya.ru>
2024-07-31 19:11:12 +0500

    изменился вывод светодиода LED1 (плата YD-CH32V307VCT6)

--- ch32v3xx/01-default/main.c
+++ ch32v3xx/01-default/main.c
@@ -4,6 +4,8 @@
 /* Типы */
 
 /* Макросы и определения  */
+#define LED_Port    GPIOA
+#define LED_Pin     GPIO_Pin_15
 
 /* Переменные */
 volatile u32 cnt = 0;
@@ -14,11 +16,11 @@ void LED1_BLINK_INIT( void )
 
     RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA, ENABLE );
 
-    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Pin   = LED_Pin;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
 
-    GPIO_Init( GPIOA, &GPIO_InitStructure );
+    GPIO_Init( LED_Port, &GPIO_InitStructure );
 }
 
 
@@ -33,13 +35,13 @@ int main( void )
     Delay_Init();
     LED1_BLINK_INIT();
 
-    GPIO_ResetBits( GPIOA, GPIO_Pin_0 );
+    GPIO_ResetBits( LED_Port, LED_Pin );
 
     while ( 1 )
     {
         cnt = ( cnt + 1 ) % 0xFFFFFF;
 
         Delay_Ms( 250 );
-        GPIO_WriteBit( GPIOA, GPIO_Pin_0, ( i == 0 ) ? ( i = Bit_SET ) : ( i = Bit_RESET ) );
+        GPIO_WriteBit( LED_Port, LED_Pin, ( i == 0 ) ? ( i = Bit_SET ) : ( i = Bit_RESET ) );
     }
 }
